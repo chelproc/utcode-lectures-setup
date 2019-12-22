4a3048dee087f07a67ca466b6255e8a98314861e
chelproc <chelproc@outlook.com>
2019-12-22 09:33:33 +0900

    Node.jsのインストールスクリプトの追加

--- lamp-on-docker/init.sh
+++ lamp-on-docker/init.sh
@@ -3,7 +3,30 @@ curl -fsSL get.docker.com | sh
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-apt -y install php-imagick php-pear php7.2 php7.2-cli php7.2-cgi php7.2-common php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mysql php7.2-opcache php7.2-readline php7.2-xml php7.2-xmlrpc php7.2-zip libapache2-mod-php7.2
+apt -y install \
+  php-imagick \
+  php-pear \
+  php7.2 \
+  php7.2-cli \
+  php7.2-cgi \
+  php7.2-common \
+  php7.2-curl \
+  php7.2-gd \
+  php7.2-json \
+  php7.2-mbstring \
+  php7.2-mysql \
+  php7.2-opcache \
+  php7.2-readline \
+  php7.2-xml \
+  php7.2-xmlrpc \
+  php7.2-zip \
+  libapache2-mod-php7.2 \
+  nodejs \
+  npm
+
+npm install -g n
+n stable
+apt purge -y nodejs npm
 
 cd /root
 mkdir docker
